edfe0c0bfb9dc81473c4838e69c0b4171be2d3fe
owen126315 <owen126315@gmail.com>
2019-06-30 23:01:57 +0800

    try

---
+++ helloWorld/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"try"
+)
+
+func main() {
+	fmt.Println("Hello world!")
+	// for i := 0; i < 100; i++ {
+	// 	// fmt.Println(i)
+	// 	switch i {
+	// 	case 10:
+	// 		fmt.Println(10)
+	// 	case 20:
+	// 		fmt.Println(try.My_str)
+	// 	}
+	// }
+	// fmt.Println(try.Add(50, 2))
+	var sum int
+	num := []int{1, 6, 3}
+	sum = try.Add_multi(num)
+	fmt.Println(sum)
+}
---
+++ try/try.go
@@ -0,0 +1,15 @@
+package try
+
+var My_str = "hi"
+
+func Add(a int, b int) int {
+	return a + b
+}
+
+func Add_multi(x []int) int {
+	var sum int
+	for _, n := range x {
+		sum += n
+	}
+	return sum
+}
